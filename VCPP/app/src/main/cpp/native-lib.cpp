27ee4b1cbe412b6c1f17b2a77df1f95f747c83c3
Visin1991 <zhuzhanhao1991@gmail.com>
2020-04-06 21:51:30 +0800

    add ffmpeg

--- VCPP/app/src/main/cpp/native-lib.cpp
+++ VCPP/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,74 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,"testff",__VA_ARGS__);
+
+extern "C"
+{
+    #include <libavcodec/avcodec.h>
+    #include <libavformat/avformat.h>
+}
+
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_vcpp_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    hello += avcodec_configuration();
+
+    //初始化解封装
+    av_register_all();
+    //初始化网络
+    avformat_network_init();
+    //打开文件
+
+    av_log_set_level(AV_LOG_DEBUG);
+
+    AVFormatContext* ic = NULL;
+    char path[] = "/sdcard/1080p.mp4";
+    int re = avformat_open_input(&ic,path,0,0);
+
+    LOGW("avformat_open_input %s !",path);
+    LOGW("avformat_open_input : %s",av_err2str(re));
+
+    if(re!=0)
+    {
+        return env->NewStringUTF(hello.c_str());
+    }
+
+    re = avformat_find_stream_info(ic,0);
+
+    if(re!=0)
+    {
+        LOGW("avformat_find_stream_info failed!");
+    }
+
+    LOGW("duration time = %lld nb_stream = %d",ic->duration,ic->nb_streams);
+
+    avformat_close_input(&ic);
+
     return env->NewStringUTF(hello.c_str());
+
+
+}
+
+extern "C"
+JNIEXPORT jboolean JNICALL
+Java_com_example_vcpp_MainActivity_Open(JNIEnv *env, jobject thiz, jstring url, jobject handle) {
+    // TODO: implement Open()
+    const char* f_url = env->GetStringUTFChars(url,0);
+    FILE* fp = fopen(f_url,"rb");
+
+    if(!fp)
+    {
+        LOGW("%s open filed!",f_url);
+    } else{
+        LOGW("%s open sucess!",f_url);
+        fclose(fp);
+    }
+
+    env->ReleaseStringUTFChars(url,f_url);
+    return true;
 }
--- VCPP/app/src/main/java/com/example/vcpp/MainActivity.java
+++ VCPP/app/src/main/java/com/example/vcpp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.vcpp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -14,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        //运行时申请读写权限
+        //verifyStoragePermissions(this);
+        //Open("/sdcard/1080p.mp4",this);
+
     }
 
     /**
@@ -21,6 +30,32 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    public native boolean Open(String url,Object handle);
+
+    //-----------------------------------------------------------------
+    // 运行时设置读写权限
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE" };
+
+    public static void verifyStoragePermissions(Activity activity) {
+
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //-----------------------------------------------------------------
+
 
     // Used to load the 'native-lib' library on application startup.
     static {
