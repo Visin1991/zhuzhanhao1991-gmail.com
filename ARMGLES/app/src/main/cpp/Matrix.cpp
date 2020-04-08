580e409c481236fd37394476b49b81e3f52c50c5
Visin1991 <zhuzhanhao1991@gmail.com>
2020-04-08 23:37:40 +0800

    finish Simple Cube

---
+++ ARMGLES/app/src/main/cpp/Matrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Visin on 4/8/2020.
+//
+
+#include "Matrix.h"
---
+++ ARMGLES/app/src/main/cpp/Matrix.h
@@ -0,0 +1,16 @@
+//
+// Created by Visin on 4/8/2020.
+//
+
+#ifndef ARMGLES_MATRIX_H
+#define ARMGLES_MATRIX_H
+
+
+
+class Matrix {
+
+};
+
+
+
+#endif //ARMGLES_MATRIX_H
