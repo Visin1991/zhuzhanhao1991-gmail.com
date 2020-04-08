2c9cec4c21fd803e3c3657bfd49e35326c410a96
Visin1991 <zhuzhanhao1991@gmail.com>
2020-04-08 23:38:16 +0800

    add simple cube

--- ARMGLES/app/src/main/cpp/native-lib.cpp
+++ ARMGLES/app/src/main/cpp/native-lib.cpp
@@ -11,6 +11,8 @@
 #include <android/log.h>
 #include <unistd.h>
 
+#include "Matrix.h"
+
 #define LOG_TAG "libNative"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
@@ -19,18 +21,25 @@
 
 //-----------------------------------------------------------------------------------------------------------
 
-static const char glVertexShader[] =
-        "attribute vec4 vPosition;\n"
+static const char  glVertexShader[] =
+        "attribute vec4 vertexPosition;\n"
+        "attribute vec3 vertexColour;\n"
+        "varying vec3 fragColour;\n"
+        "uniform mat4 projection;\n"
+        "uniform mat4 modelView;\n"
         "void main()\n"
         "{\n"
-        "  gl_Position = vPosition;\n"
+        "    gl_Position = projection * modelView * vertexPosition;\n"
+        "    fragColour = vertexColour;\n"
         "}\n";
 
-static const char glFragmentShader[] =
+
+static const char  glFragmentShader[] =
         "precision mediump float;\n"
+        "varying vec3 fragColour;\n"
         "void main()\n"
         "{\n"
-        "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "    gl_FragColor = vec4(fragColour, 1.0);\n"
         "}\n";
 
 GLuint loadShader(GLenum shaderType, const char* shaderSource)
@@ -108,36 +117,112 @@ GLuint createProgram(const char* vertexSource, const char * fragmentSource)
     return program;
 }
 
-GLuint simpleTriangleProgram;
-GLuint vPosition;
+GLuint simpleCubeProgram;
+GLuint vertexLocation;
+GLuint vertexColourLocation;
+GLuint projectionLocation;
+GLuint modelViewLocation;
 
-bool setupGraphics(int w, int h)
+float projectionMatrix[16];
+float modelViewMatrix[16];
+float angle = 0;
+
+bool setupGraphics(int width, int height)
 {
-    simpleTriangleProgram = createProgram(glVertexShader, glFragmentShader);
-    if (!simpleTriangleProgram)
+    simpleCubeProgram = createProgram(glVertexShader, glFragmentShader);
+    if (simpleCubeProgram == 0)
     {
         LOGE ("Could not create program");
         return false;
     }
-    vPosition = glGetAttribLocation(simpleTriangleProgram, "vPosition");
-    glViewport(0, 0, w, h);
+    vertexLocation = glGetAttribLocation(simpleCubeProgram, "vertexPosition");
+    vertexColourLocation = glGetAttribLocation(simpleCubeProgram, "vertexColour");
+    projectionLocation = glGetUniformLocation(simpleCubeProgram, "projection");
+    modelViewLocation = glGetUniformLocation(simpleCubeProgram, "modelView");
+    /* Setup the perspective */
+    matrixPerspective(projectionMatrix, 45, (float)width / (float)height, 0.1f, 100);
+    glEnable(GL_DEPTH_TEST);
+    glViewport(0, 0, width, height);
     return true;
 }
 
-const GLfloat triangleVertices[] = {
-        0.0f, 1.0f,
-        -1.0f, -1.0f,
-        1.0f, -1.0f
+GLfloat cubeVertices[] = {-1.0f,  1.0f, -1.0f, /* Back. */
+                          1.0f,  1.0f, -1.0f,
+                          -1.0f, -1.0f, -1.0f,
+                          1.0f, -1.0f, -1.0f,
+                          -1.0f,  1.0f,  1.0f, /* Front. */
+                          1.0f,  1.0f,  1.0f,
+                          -1.0f, -1.0f,  1.0f,
+                          1.0f, -1.0f,  1.0f,
+                          -1.0f,  1.0f, -1.0f, /* Left. */
+                          -1.0f, -1.0f, -1.0f,
+                          -1.0f, -1.0f,  1.0f,
+                          -1.0f,  1.0f,  1.0f,
+                          1.0f,  1.0f, -1.0f, /* Right. */
+                          1.0f, -1.0f, -1.0f,
+                          1.0f, -1.0f,  1.0f,
+                          1.0f,  1.0f,  1.0f,
+                          -1.0f, -1.0f, -1.0f, /* Top. */
+                          -1.0f, -1.0f,  1.0f,
+                          1.0f, -1.0f,  1.0f,
+                          1.0f, -1.0f, -1.0f,
+                          -1.0f,  1.0f, -1.0f, /* Bottom. */
+                          -1.0f,  1.0f,  1.0f,
+                          1.0f,  1.0f,  1.0f,
+                          1.0f,  1.0f, -1.0f
 };
+
+GLfloat colour[] = {1.0f, 0.0f, 0.0f,
+                    1.0f, 0.0f, 0.0f,
+                    1.0f, 0.0f, 0.0f,
+                    1.0f, 0.0f, 0.0f,
+                    0.0f, 1.0f, 0.0f,
+                    0.0f, 1.0f, 0.0f,
+                    0.0f, 1.0f, 0.0f,
+                    0.0f, 1.0f, 0.0f,
+                    0.0f, 0.0f, 1.0f,
+                    0.0f, 0.0f, 1.0f,
+                    0.0f, 0.0f, 1.0f,
+                    0.0f, 0.0f, 1.0f,
+                    1.0f, 1.0f, 0.0f,
+                    1.0f, 1.0f, 0.0f,
+                    1.0f, 1.0f, 0.0f,
+                    1.0f, 1.0f, 0.0f,
+                    0.0f, 1.0f, 1.0f,
+                    0.0f, 1.0f, 1.0f,
+                    0.0f, 1.0f, 1.0f,
+                    0.0f, 1.0f, 1.0f,
+                    1.0f, 0.0f, 1.0f,
+                    1.0f, 0.0f, 1.0f,
+                    1.0f, 0.0f, 1.0f,
+                    1.0f, 0.0f, 1.0f
+};
+
+GLushort indices[] = {0, 2, 3, 0, 1, 3, 4, 6, 7, 4, 5, 7, 8, 9, 10, 11, 8, 10, 12, 13, 14, 15, 12, 14, 16, 17, 18, 16, 19, 18, 20, 21, 22, 20, 23, 22};
+
 
 void renderFrame()
 {
+
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear (GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-    glUseProgram(simpleTriangleProgram);
-    glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0 ,triangleVertices);
-    glEnableVertexAttribArray(vPosition);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+    matrixIdentityFunction(modelViewMatrix);
+    matrixRotateX(modelViewMatrix, angle);
+    matrixRotateY(modelViewMatrix, angle);
+    matrixTranslate(modelViewMatrix, 0.0f, 0.0f, -10.0f);
+    glUseProgram(simpleCubeProgram);
+    glVertexAttribPointer(vertexLocation, 3, GL_FLOAT, GL_FALSE, 0, cubeVertices);
+    glEnableVertexAttribArray(vertexLocation);
+    glVertexAttribPointer(vertexColourLocation, 3, GL_FLOAT, GL_FALSE, 0, colour);
+    glEnableVertexAttribArray(vertexColourLocation);
+    glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, projectionMatrix);
+    glUniformMatrix4fv(modelViewLocation, 1, GL_FALSE, modelViewMatrix);
+    glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, indices);
+    angle += 1;
+    if (angle > 360)
+    {
+        angle -= 360;
+    }
 }
 
 //--------------------------------------------------------------------------------------------------------------
